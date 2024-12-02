1bd321c557f05fcb4736d63ae70e030baf01f28f
Aidan Pilato <aidan@red-bean.com>
2024-12-02 16:07:38 -0500

    Added customers and reviews to index.py

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, ingredients, menu_items, payment, promotion
+from . import orders, order_details, ingredients, menu_items, payment, promotion, customers, reviews
 
 
 def load_routes(app):
@@ -8,5 +8,7 @@ def load_routes(app):
     app.include_router(menu_items.router)
     app.include_router(payment.router)
     app.include_router(promotion.router)
+    app.include_router(customers.router)
+    app.include_router(reviews.router)
 
 
